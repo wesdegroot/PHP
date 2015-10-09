b0a8629572d81efba119a5376361db1a27b0853f
Wesley de Groot <wes@vista.aero>
2015-10-09 23:30:09 +0200

    Dataset

---
+++ datasets/x.php
@@ -0,0 +1,1422 @@
+<?php
+
+$ex = "Aachen Hbf (AHBF)
+
+Aachen Rothe Erde (ARE)
+
+Aachen Schanz (ASCH)
+
+Aachen West (AW)
+
+Aalten (ATN)
+
+Abcoude (AC)
+
+Ahaus (EAHS)
+
+Aime-la-Plagne (AIME)
+
+Aix-en-Provence (AIXTGV)
+
+Akkrum (AKM)
+
+Albertville (ALBERT)
+
+Alessandria (ALESS)
+
+Alkmaar (AMR)
+
+Alkmaar Noord (AMRN)
+
+Almelo (AML)
+
+Almelo de Riet (AMRI)
+
+Almere Buiten (ALMB)
+
+Almere Centrum (ALM)
+
+Almere Muziekwijk (ALMM)
+
+Almere Oostvaarders (ALMO)
+
+Almere Parkwijk (ALMP)
+
+Almere Poort (AMPO)
+
+Alphen a/d Rijn (APN)
+
+Altenberge (EABG)
+
+Amersfoort (AMF)
+
+Amersfoort Schothorst (AMFS)
+
+Amersfoort Vathorst (AVAT)
+
+Amsterdam Amstel (ASA)
+
+Amsterdam Bijlmer ArenA (ASB)
+
+Amsterdam Centraal (ASD)
+
+Amsterdam Holendrecht (ASHD)
+
+Amsterdam Lelylaan (ASDL)
+
+Amsterdam Muiderpoort (ASDM)
+
+Amsterdam RAI (RAI)
+
+Amsterdam Science Park (ASSP)
+
+Amsterdam Sloterdijk (ASS)
+
+Amsterdam Van der Madeweg (VMW)
+
+Amsterdam Zuid (ASDZ)
+
+Anna Paulowna (ANA)
+
+Antwerpen-Berchem (BERCH)
+
+Antwerpen-Centraal (ATW)
+
+Antwerpen-Luchtbal (ATWLB)
+
+Antwerpen-Noorderdokken (ANDD)
+
+Antwerpen-Zuid (FNZD)
+
+Apeldoorn (APD)
+
+Apeldoorn De Maten (APDM)
+
+Apeldoorn Osseveld (APDO)
+
+Appingedam (APG)
+
+Arkel (AKL)
+
+Arnemuiden (ARN)
+
+Arnhem (AH)
+
+Arnhem Presikhaaf (AHPR)
+
+Arnhem Velperpoort (AHP)
+
+Arnhem Zuid (AHZ)
+
+Ashford International (ASHFD)
+
+Assen (ASN)
+
+Augsburg Hbf (MA)
+
+Avignon TGV (AVTGV)
+
+Leeuwarden Achter d Hoven (ADH)
+
+Rotterdam Alexander (RTA)
+
+Schiphol (SHL)
+
+Almere Buiten (ALMB)
+
+Amsterdam Bijlmer ArenA (ASB)
+
+Baarn (BRN)
+
+Bad Bentheim (BH)
+
+Bad Nieuweschans (NSCH)
+
+Bad Oeynhausen (OEYNH)
+
+Bad Schandau (BADSCH)
+
+Baden (BADENZ)
+
+Baden-Baden (RBB)
+
+Baflo (BF)
+
+Barendrecht (BRD)
+
+Barneveld Centrum (BNC)
+
+Barneveld Noord (BNN)
+
+Barneveld Zuid (BNZ)
+
+Basel Bad Bf (BASELB)
+
+Basel SBB (BASELS)
+
+Bedum (BDM)
+
+Beek-Elsloo (BK)
+
+Beesd (BSD)
+
+Beilen (BL)
+
+Bergen op Zoom (BGN)
+
+Berlin Hbf (BERHBL)
+
+Berlin Hbf (tief) (BERHBT)
+
+Berlin Ostbahnhof (BHF)
+
+Berlin Südkreuz (BPAF)
+
+Berlin-Schönefeld Flug. (BERFL)
+
+Berlin-Spandau (BSPD)
+
+Best (BET)
+
+Beverwijk (BV)
+
+Bielefeld Hbf (BIELEF)
+
+Bilthoven (BHV)
+
+Bilzen (FIE)
+
+Bischofshofen (BISCHO)
+
+Blerick (BR)
+
+Bloemendaal (BLL)
+
+Bludenz (BLUDEN)
+
+Bodegraven (BDG)
+
+Boisheim (KBOI)
+
+Bonn Hbf (BONN)
+
+Boom (FMB)
+
+Bork (EBOK)
+
+Borne (BN)
+
+Boskoop (BSK)
+
+Bourg-St-Maurice (BOURG)
+
+Boven-Hardinxveld (BHDV)
+
+Bovenkarspel Flora (BKF)
+
+Bovenkarspel-Grootebroek (BKG)
+
+Boxmeer (BMR)
+
+Boxtel (BTL)
+
+Braunschweig Hbf (BRAUNS)
+
+Breda (BD)
+
+Breda-Prinsenbeek (BDPB)
+
+Bressoux (BRESSX)
+
+Breukelen (BKL)
+
+Breyell (KBRY)
+
+Brummen (BMN)
+
+Brussel Nat. Luchthaven (FBNL)
+
+Brussel-Centraal (BRUSC)
+
+Brussel-Noord (BRUSN)
+
+Brussel-Zuid/Midi (BRUSZ)
+
+Buitenpost (BP)
+
+Bunde (BDE)
+
+Bunnik (BNK)
+
+Bussum Zuid (BSMZ)
+
+Bönen (EBEO)
+
+Bünde (Westf) (BUENDE)
+
+Eindhoven Beukenlaan (EHB)
+
+Hardinxveld Blauwe Zoom (HBZM)
+
+Helmond Brandevoort (HMBV)
+
+Helmond Brouwhuis (HMBH)
+
+Rotterdam Blaak (RTB)
+
+Sliedrecht Baanhoek (SDTB)
+
+Capelle Schollevaar (CPS)
+
+Castricum (CAS)
+
+Chambéry (CHAMB)
+
+Chevremont (CVM)
+
+Chur (CHUR)
+
+Coesfeld (Westf) (ECMF)
+
+Coevorden (CO)
+
+Como (COMO)
+
+Cuijk (CK)
+
+Culemborg (CL)
+
+Deventer Colmschate (DVC)
+
+Houten Castellum (HTNC)
+
+Leeuwarden Camminghaburen (LWC)
+'s-Hertogenbosch (HT)
+
+'s-Hertogenbosch Oost (HTO)
+
+Almelo de Riet (AMRI)
+
+Apeldoorn De Maten (APDM)
+
+Daarlerveen (DA)
+
+Dalen (DLN)
+
+Dalfsen (DL)
+
+De Vink (DVNK)
+
+Decin hl.n. (DECIN)
+
+Deinum (DEI)
+
+Delden (DDN)
+
+Delft (DT)
+
+Delft Zuid (DTZ)
+
+Delfzijl (DZ)
+
+Delfzijl West (DZW)
+
+Den Dolder (DLD)
+
+Den Haag Centraal (GVC)
+
+Den Haag HS (GV)
+
+Den Haag Laan v NOI (LAA)
+
+Den Haag Mariahoeve (GVM)
+
+Den Haag Moerwijk (GVMW)
+
+Den Haag Ypenburg (YPB)
+
+Den Helder (HDR)
+
+Den Helder Zuid (HDRZ)
+
+Deurne (DN)
+
+Deventer (DV)
+
+Deventer Colmschate (DVC)
+
+Didam (DID)
+
+Diemen (DMN)
+
+Diemen Zuid (DMNZ)
+
+Diepenbeek (GDP)
+
+Dieren (DR)
+
+Doetinchem (DTC)
+
+Doetinchem De Huet (DTCH)
+
+Domodossola (DOMOD)
+
+Dordrecht (DDR)
+
+Dordrecht Stadspolders (DDRS)
+
+Dordrecht Zuid (DDZD)
+
+Dortmund Derne (EDD)
+
+Dortmund Hbf (EDO)
+
+Dortmund Kirchderne (EDKD)
+
+Dresden Hbf (DRESD)
+
+Dresden-Neustadt (DRESDN)
+
+Driebergen-Zeist (DB)
+
+Driehuis (DRH)
+
+Dronrijp (DRP)
+
+Dronten (DRON)
+
+Duffel (FDP)
+
+Duisburg Hbf (DUISB)
+
+Duiven (DVN)
+
+Duivendrecht (DVD)
+
+Dülken (KDUL)
+
+Dülmen (EDULH)
+
+Düren (KDR)
+
+Düsseldorf Hbf (DUSSEL)
+
+Enschede De Eschmarke (ESE)
+
+Enschede Drienerlo (ESD)
+
+Heerlen De Kissel (HRLK)
+
+Nijmegen Dukenburg (NMD)
+
+Veenendaal-De Klomp (KLP)
+
+Ebbsfleet International (EBBS)
+
+Echt (EC)
+
+Ede Centrum (EDC)
+
+Ede-Wageningen (ED)
+
+Eijs-Wittem (WTM)
+
+Eijsden (EDN)
+
+Eilendorf (EDF)
+
+Eindhoven (EHV)
+
+Eindhoven Beukenlaan (EHB)
+
+Ekeren (LKR)
+
+Elst (EST)
+
+Emmen (EMN)
+
+Emmen Zuid (EMNZ)
+
+Emmerich (EM)
+
+Enkhuizen (EKZ)
+
+Ennepetal (Gevelsberg) (EENP)
+
+Enschede (ES)
+
+Enschede De Eschmarke (ESE)
+
+Enschede Drienerlo (ESD)
+
+Epe (Westf) (EEPE)
+
+Ermelo (EML)
+
+Eschweiler Hbf (ESCH)
+
+Eschweiler Talbahnhof (ESCHT)
+
+Eschweiler-Nothberg (ESCHN)
+
+Eschweiler-Weisweiler (EWEIS)
+
+Eschweiler-West (ESCHW)
+
+Essen (Belgie) (ESN)
+
+Etten-Leur (ETN)
+
+Eygelshoven (EGH)
+
+Eygelshoven Markt (EGHM)
+
+Groningen Europapark (GERP)
+
+Fieberbrunn (FIEBER)
+
+Franeker (FN)
+
+Frankfurt (M) Hbf (FFFM)
+
+Frankfurt (Oder) (FRANKO)
+
+Frankfurt Flughafen Fernb (FNAF)
+
+Freiburg (Breisgau) Hbf (RF)
+
+Gaanderen (GDR)
+
+Geerdijk (GDK)
+
+Geislingen (GEISL)
+
+Geldermalsen (GDM)
+
+Geldrop (GP)
+
+Geleen Oost (GLN)
+
+Geleen-Lutterade (LUT)
+
+Gilze-Rijen (GZ)
+
+Glanerbrug (GBR)
+
+Glons (LGL)
+
+Goes (GS)
+
+Goor (GO)
+
+Gorinchem (GR)
+
+Gouda (GD)
+
+Gouda Goverwelle (GDG)
+
+Gramsbergen (GBG)
+
+Grijpskerk (GK)
+
+Gronau (Westf.) (G)
+
+Groningen (GN)
+
+Groningen Europapark (GERP)
+
+Groningen Noord (GNN)
+
+Grou-Jirnsum (GW)
+
+Gulpen-Wijlre (WIJ)
+
+Göppingen (TGO)
+
+Günzburg (MGZB)
+
+Nijmegen Goffert (NMGO)
+
+'s-Hertogenbosch (HT)
+
+'s-Hertogenbosch Oost (HTO)
+
+'t Harde (HDE)
+
+Amsterdam Holendrecht (ASHD)
+
+Haarlem (HLM)
+
+Haarlem Spaarnwoude (HLMS)
+
+Hagen Hbf (HAGEN)
+
+Halfweg-Zwanenburg (HWZB)
+
+Hamburg Hbf (HAMH)
+
+Hamm (Westf.) (HAMM)
+
+Hannover Hbf (HANN)
+
+Hardenberg (HDB)
+
+Harderwijk (HD)
+
+Hardinxveld Blauwe Zoom (HBZM)
+
+Hardinxveld-Giessendam (GND)
+
+Haren (HRN)
+
+Harlingen (HLG)
+
+Harlingen Haven (HLGH)
+
+Hasselt (FHS)
+
+Heemskerk (HK)
+
+Heemstede-Aerdenhout (HAD)
+
+Heerenveen (HR)
+
+Heerenveen IJsstadion (HRY)
+
+Heerhugowaard (HWD)
+
+Heerlen (HRL)
+
+Heerlen De Kissel (HRLK)
+
+Heerlen Woonboulevard (HRLW)
+
+Heeze (HZE)
+
+Heide (MID)
+
+Heiloo (HLO)
+
+Heino (HNO)
+
+Helmond (HM)
+
+Helmond 't Hout (HMH)
+
+Helmond Brandevoort (HMBV)
+
+Helmond Brouwhuis (HMBH)
+
+Hemiksem (LKM)
+
+Hemmen-Dodewaard (HMN)
+
+Hengelo (HGL)
+
+Hengelo Gezondheidspark (HGLG)
+
+Hengelo Oost (HGLO)
+
+Herstal (LHS)
+
+Herzogenrath (HZ)
+
+Hillegom (HIL)
+
+Hilversum (HVS)
+
+Hilversum Media Park (HVSM)
+
+Hilversum Sportpark (HVSP)
+
+Hindeloopen (HNP)
+
+Hoboken-Polder (FOE)
+
+Hoek van Holland Haven (HLD)
+
+Hoek van Holland Strand (HLDS)
+
+Hoensbroek (HB)
+
+Hoevelaken (HVL)
+
+Hollandsche Rading (HOR)
+
+Holten (HON)
+
+Holzwickede (EHZW)
+
+Hoofddorp (HFD)
+
+Hoogeveen (HGV)
+
+Hoogezand-Sappemeer (HGZ)
+
+Hoogkarspel (HKS)
+
+Hoorn (HN)
+
+Hoorn Kersenboogerd (HNK)
+
+Hopfgarten (HOPF)
+
+Horst-Sevenum (HRT)
+
+Houten (HTN)
+
+Houten Castellum (HTNC)
+
+Houthem-St Gerlach (SGL)
+
+Hove (MHO)
+
+Hurdegaryp (HDG)
+
+Nijmegen Heyendaal (NMH)
+
+Heerenveen IJsstadion (HRY)
+
+IJlst (IJT)
+
+Innsbruck Hbf (INNSB)
+
+Jenbach (JENBAC)
+
+Hoorn Kersenboogerd (HNK)
+
+Kaldenkirchen (KN)
+
+Kalmthout (FKTH)
+
+Kampen (KPN)
+
+Kampen Zuid (KPNZ)
+
+Kapelle-Biezelinge (BZL)
+
+Kapellen (LKP)
+
+Karlsruhe Hbf (KARLSR)
+
+Kerkrade Centrum (KRD)
+
+Kerkrade West (KRW)
+
+Kesteren (KTR)
+
+Kijkuit (GKT)
+
+Kirchberg in Tirol (KIRCHB)
+
+Kitzbühel (KITZB)
+
+Klarenbeek (KBK)
+
+Klimmen-Ransdaal (KMR)
+
+Koblenz Hbf (KKO)
+
+Kohlscheid (KOHL)
+
+Konin (KONIN)
+
+Kontich (FKI)
+
+Koog Bloemwijk (KBW)
+
+Koog-Zaandijk (KZD)
+
+Koudum-Molkwerum (KMW)
+
+Krabbendijke (KBD)
+
+Krommenie-Assendelft (KMA)
+
+Kropswolde (KW)
+
+Kruiningen-Yerseke (KRG)
+
+Kufstein (KUFST)
+
+Kutno (KUTNO)
+
+Köln Hbf (KOLN)
+
+Köln-Deutz (KKD)
+
+Zaandam Kogerveld (ZDK)
+
+Amsterdam Lelylaan (ASDL)
+
+Den Haag Laan v NOI (LAA)
+
+Lage Zwaluwe (ZLW)
+
+Landeck (LAND)
+
+Landgraaf (LG)
+
+Landquart (LANDQ)
+
+Landry (LANDRY)
+
+Langerwehe (LANG)
+
+Leer (Ostfriesland) (LEER)
+
+Leerdam (LDM)
+
+Leeuwarden (LW)
+
+Leeuwarden Achter d Hoven (ADH)
+
+Leeuwarden Camminghaburen (LWC)
+
+Legden (ELGD)
+
+Leiden Centraal (LEDN)
+
+Leiden Lammenschans (LDL)
+
+Lelystad Centrum (LLS)
+
+Lette (Kr Coersfeld) (ELET)
+
+Leuven (LEUVEN)
+
+Lichtenvoorde-Groenlo (LTV)
+
+Liers (LSL)
+
+Lille Europe (LILLEE)
+
+Limburg Süd (FLB)
+
+Livorno Centrale (LIVOR)
+
+Liège-Guillemins (LUIK)
+
+Liège-Jonfosse (LUIKJ)
+
+Liège-Palais (LUIKP)
+
+Lochem (LC)
+
+London St. Pancras (LONDS)
+
+Loppersum (LP)
+
+Lunteren (LTN)
+
+Lüdinghausen (ELDH)
+
+Lünen Hbf (ELUE)
+
+Nijmegen Lent (NML)
+
+Rotterdam Lombardijen (RLB)
+
+Utrecht Leidsche Rijn (UTLR)
+
+Utrecht Lunetten (UTL)
+
+Almere Muziekwijk (ALMM)
+
+Amsterdam Muiderpoort (ASDM)
+
+Den Haag Mariahoeve (GVM)
+
+Den Haag Moerwijk (GVMW)
+
+Hilversum Media Park (HVSM)
+
+Maarheeze (MZ)
+
+Maarn (MRN)
+
+Maarssen (MAS)
+
+Maassluis (MSS)
+
+Maassluis West (MSW)
+
+Maastricht (MT)
+
+Maastricht Noord (MTN)
+
+Maastricht Randwyck (MTR)
+
+Mannheim Hbf (MANNHE)
+
+Mantgum (MG)
+
+Mariënberg (MRB)
+
+Marne-la-Vallée-Chessy (MARNE)
+
+Marseille-St-Charles (MARS)
+
+Martenshoek (MTH)
+
+Mechelen (MECH)
+
+Mechelen-Nekkerspoel (LNK)
+
+Meerssen (MES)
+
+Meppel (MP)
+
+Metelen Land (EMTE)
+
+Middelburg (MDB)
+
+Milmort (BLTM)
+
+Minden (Westf.) (MINDEN)
+
+Montabaur (KMB)
+
+Mook Molenhoek (MMLH)
+
+Mortsel-Deurnesteenweg (GMD)
+
+Mortsel-Oude God (GMOG)
+
+Moutiers-Salins-Brides (MOUT)
+
+Mönchengladbach Hbf (MCGB)
+
+München Hbf (MUNCHH)
+
+Münster (Westf.) Hbf (MUNST)
+
+Münster Zentrum Nord (ENHF)
+
+Münster-Häger (ENBE)
+
+Utrecht Maliebaan (UTM)
+Bad Nieuweschans (NSCH)
+
+Mechelen-Nekkerspoel (LNK)
+
+Naarden-Bussum (NDB)
+
+Neuss Hbf (NEUSS)
+
+Niel (LNI)
+
+Nieuw Amsterdam (NA)
+
+Nieuw Vennep (NVP)
+
+Nieuwerkerk a/d IJssel (NWK)
+
+Nijkerk (NKK)
+
+Nijmegen (NM)
+
+Nijmegen Dukenburg (NMD)
+
+Nijmegen Goffert (NMGO)
+
+Nijmegen Heyendaal (NMH)
+
+Nijmegen Lent (NML)
+
+Nijverdal (NVD)
+
+Noorderkempen (NDKP)
+
+Nordwalde (ENOW)
+
+Nunspeet (NS)
+
+Nuth (NH)
+
+Nürnberg Hbf (NURNB)
+
+Schiedam Nieuwland (NWL)
+Almere Oostvaarders (ALMO)
+
+Apeldoorn Osseveld (APDO)
+
+Obdam (OBD)
+
+Oberhausen Hbf (OBERH)
+
+Ochtrup (EOP)
+
+Offenburg (RO)
+
+Oisterwijk (OT)
+
+Oldenzaal (ODZ)
+
+Olst (OST)
+
+Ommen (OMN)
+
+Oosterbeek (OTB)
+
+Opheusden (OP)
+
+Osnabrück Hbf (OSNH)
+
+Oss (O)
+
+Oss West (OW)
+
+Oudenbosch (ODB)
+
+Overveen (OVN)
+
+Purmerend Overwhere (PMO)
+
+Utrecht Overvecht (UTO)
+
+Ötztal (OETZ)
+Almere Parkwijk (ALMP)
+
+Arnhem Presikhaaf (AHPR)
+
+Breda-Prinsenbeek (BDPB)
+
+Paris-Nord (PARIS)
+
+Plochingen (TP)
+
+Poznan Gl. (POZN)
+
+Praha hl.n. (PRAHA)
+
+Praha-Holesovice (PRAHOL)
+
+Preussen (EPRN)
+
+Purmerend (PMR)
+
+Purmerend Overwhere (PMO)
+
+Purmerend Weidevenne (PMW)
+
+Putten (PT)
+
+Puurs (FRP)
+
+Tiel Passewaaij (TPSW)
+
+Raalte (RAT)
+
+Rathenow (LRW)
+
+Ravenstein (RVS)
+
+Reuver (RV)
+
+Rheden (RH)
+
+Rheine (RHEINE)
+
+Rhenen (RHN)
+
+Rijssen (RSN)
+
+Rijswijk (RSW)
+
+Rilland-Bath (RB)
+
+Roermond (RM)
+
+Roodeschool (RD)
+
+Roosendaal (RSD)
+
+Rosendahl-Holtwick (EHW)
+
+Rosmalen (RS)
+
+Rotterdam Alexander (RTA)
+
+Rotterdam Blaak (RTB)
+
+Rotterdam Centraal (RTD)
+
+Rotterdam Lombardijen (RLB)
+
+Rotterdam Noord (RTN)
+
+Rotterdam Stadion (RTST)
+
+Rotterdam Zuid (RTZ)
+
+Ruisbroek-Sauvegarde (FSV)
+
+Ruurlo (RL)
+
+Rzepin (RZEPIN)
+
+Tilburg Reeshof (TBR)
+
+Amersfoort Schothorst (AMFS)
+
+Amsterdam Science Park (ASSP)
+
+Amsterdam Sloterdijk (ASS)
+
+Capelle Schollevaar (CPS)
+
+Dordrecht Stadspolders (DDRS)
+
+Haarlem Spaarnwoude (HLMS)
+
+Hilversum Sportpark (HVSP)
+
+Saalfelden (SAALF)
+
+Santpoort Noord (SPTN)
+
+Santpoort Zuid (SPTZ)
+
+Sappemeer Oost (SPM)
+
+Sassenheim (SSH)
+
+Sauwerd (SWD)
+
+Schagen (SGN)
+
+Scheemda (SDA)
+
+Schelle (SCHLL)
+
+Schiedam Centrum (SDM)
+
+Schiedam Nieuwland (NWL)
+
+Schin op Geul (SOG)
+
+Schinnen (SN)
+
+Schiphol (SHL)
+
+Schwarzach-St. Veit (SCHWAR)
+
+Schwelm (KSWE)
+
+Schwerte (Ruhr) (ESRT)
+
+Selm (ESEM)
+
+Selm-Beifang (ESEB)
+
+Siegburg/Bonn (KSB)
+
+Simpelveld (SPV)
+
+Sint-Katelijne-Waver (FWA)
+
+Sittard (STD)
+
+Sliedrecht (SDT)
+
+Sliedrecht Baanhoek (SDTB)
+
+Sneek (SK)
+
+Sneek Noord (SKND)
+
+Soest (ST)
+
+Soest Zuid (STZ)
+
+Soestdijk (SD)
+
+Spaubeek (SBK)
+
+Spekholzerheide (SPH)
+
+St-Mariaburg (MARIA)
+
+St. Johann im Pongau (STJOHP)
+
+St. Johann in Tirol (STJOH)
+
+Stavoren (STV)
+
+Stedum (STM)
+
+Steenwijk (SWK)
+
+Steinfurt Grottenkamp (EGRK)
+
+Steinfurt-Borghorst (EBGO)
+
+Steinfurt-Burgsteinfurt (EBFT)
+
+Stendal (LS)
+
+Stolberg Altstadt (STOLA)
+
+Stolberg GBF (STOLGB)
+
+Stolberg Hbf (STOLB)
+
+Stolberg Mühlener Bahnhof (STOLMB)
+
+Stolberg Rathaus (STOLR)
+
+Stolberg Schneidemuhle (STOLSM)
+
+Stuttgart Hbf (TS)
+
+Susteren (SRN)
+
+Swalmen (SM)
+
+Utrecht Maliebaan (UTM)
+
+Vlissingen Souburg (VSS)
+
+Tantow (WTA)
+
+Tegelen (TG)
+
+Terborg (TBG)
+
+Tiel (TL)
+
+Tiel Passewaaij (TPSW)
+
+Tilburg (TB)
+
+Tilburg Reeshof (TBR)
+
+Tilburg Universiteit (TBU)
+
+Tongeren (FTG)
+
+Twello (TWL)
+
+Utrecht Terwijde (UTT)
+
+Uitgeest (UTG)
+
+Uithuizen (UHZ)
+
+Uithuizermeeden (UHM)
+
+Ulm Hbf (TU)
+
+Unna (EUN)
+
+Usquert (UST)
+
+Usti nad Labem (USTI)
+
+Utrecht Centraal (UT)
+
+Utrecht Leidsche Rijn (UTLR)
+
+Utrecht Lunetten (UTL)
+
+Utrecht Maliebaan (UTM)
+
+Utrecht Overvecht (UTO)
+
+Utrecht Terwijde (UTT)
+
+Utrecht Zuilen (UTZL)
+
+Amersfoort Vathorst (AVAT)
+
+Amsterdam Van der Madeweg (VMW)
+
+Arnhem Velperpoort (AHP)
+
+Valence TGV (VALTGV)
+
+Valkenburg (VK)
+
+Varsseveld (VSV)
+
+Veendam (VDM)
+
+Veenendaal Centrum (VNDC)
+
+Veenendaal West (VNDW)
+
+Veenendaal-De Klomp (KLP)
+
+Veenwouden (VWD)
+
+Velp (VP)
+
+Venlo (VL)
+
+Venray (VRY)
+
+Vierlingsbeek (VLB)
+
+Viersen (VIERS)
+
+Visé (FVS)
+
+Vlaardingen Centrum (VDG)
+
+Vlaardingen Oost (VDO)
+
+Vlaardingen West (VDW)
+
+Vleuten (VTN)
+
+Vlissingen (VS)
+
+Vlissingen Souburg (VSS)
+
+Voerendaal (VDL)
+
+Voorburg (VB)
+
+Voorhout (VH)
+
+Voorschoten (VST)
+
+Voorst-Empe (VEM)
+
+Vorden (VD)
+
+Vriezenveen (VZ)
+
+Vroomshoop (VHP)
+
+Vught (VG)
+
+Purmerend Weidevenne (PMW)
+
+Waddinxveen (WAD)
+
+Waddinxveen Noord (WADN)
+
+Warffum (WFM)
+
+Warszawa Centralna (WARSZC)
+
+Warszawa Wschodnia (WARSAW)
+
+Weener (WR)
+
+Weert (WT)
+
+Weesp (WP)
+
+Wehl (WL)
+
+Westervoort (WTV)
+
+Wezep (WZ)
+
+Wien Westbahnhof (WIENW)
+
+Wierden (WDN)
+
+Wijchen (WC)
+
+Wijhe (WH)
+
+Wildert (GWD)
+
+Winschoten (WS)
+
+Winsum (WSM)
+
+Winterswijk (WW)
+
+Winterswijk West (WWW)
+
+Woerden (WD)
+
+Wolfheze (WF)
+
+Wolfsburg (HWOB)
+
+Wolvega (WV)
+
+Workum (WK)
+
+Wormerveer (WM)
+
+Wuppertal Barmen (KWBA)
+
+Wuppertal Hbf (WUPPH)
+
+Wuppertal Oberbarmen (KWO)
+
+Wuppertal-Vohwinkel (WUPPV)
+
+Wörgl (WORGL)
+
+Würzburg Hbf (WURZB)
+
+Den Haag Ypenburg (YPB)
+
+Utrecht Zuilen (UTZL)
+
+Zaandam (ZD)
+
+Zaandam Kogerveld (ZDK)
+
+Zaltbommel (ZBM)
+
+Zandvoort aan Zee (ZVT)
+
+Zell am See (ZELL)
+
+Zetten-Andelst (ZA)
+
+Zevenaar (ZV)
+
+Zevenbergen (ZVB)
+
+Zoetermeer (ZTM)
+
+Zoetermeer Oost (ZTMO)
+
+Zuidbroek (ZB)
+
+Zuidhorn (ZH)
+
+Zutphen (ZP)
+
+Zwaagwesteinde (ZWW)
+
+Zwijndrecht (ZWD)
+
+Zwolle (ZL)
+
+Zürich HB (ZUE)";
+
+$newArray=array();
+
+$ex = explode("\n", $ex);
+
+for ( $i=0; $i<sizeof($ex); $i++ )
+{
+	if (!empty($ex[$i]))
+	{
+		$ex2 = explode("(", $ex[$i]);
+		$newArray[] = array(
+						"station" => substr($ex2[0], 0, -1),
+						"short"   => substr($ex2[1], 0, -1),
+						"address" => substr($ex2[0], 0, -1),
+						"gMaps"   => "https://www.google.nl/maps/place/Station+" . urlencode(substr($ex2[0], 0, -1)),
+						"added"   => @date("d-m-Y H:i:s"),
+						"rPid"    => uniqid(),
+						"mPid"    => sha1(uniqid()),
+						"sPid"    => md5(uniqid())
+					   );
+	}
+}
+
+file_put_contents("Train_Public_transport_NL.php", "<?php\r\n\r\n\$TrainStationsNL = " . var_export($newArray, true) . ";\r\n\r\n?>");
+?>
